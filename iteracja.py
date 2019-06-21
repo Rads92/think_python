da5e064c78bc98f58527925cdb9d3cd5b1578cf0
Radosław Strojny <rads.strojny@gmail.com>
2019-06-21 07:57:10 +0200

    Update iteracja.py

--- iteracja.py
+++ iteracja.py
@@ -2,18 +2,11 @@ import math
 
 
 def mysqrt(a):
-    if (a == 1):
-        return 1
-    else:
-        x = a - 1
+    x = a
 
     while True:
-        
-        y = ((x + a / x) / 2)
-        print(y)
-        break
-        if abs(x - y) < 0.001:
-            print(y)
+        y = ((x + (a / x)) / 2)
+        if abs(x - y) < 0.00001:
             break
         else:
             x = y
@@ -22,11 +15,50 @@ def mysqrt(a):
 
 
 def test_square_root():
-    print("{: >20} {: >20} {: >20} {: >20}".format(
-        'a', 'mysqrt', 'math.sqrt', 'diff'))
+    print("{: >2} {: >20} {: >20} {: >25}".format('a', 'mysqrt', 'math.sqrt',
+                                                  'diff'))
     for i in range(1, 10):
-        print("{: >20} {: >20} {: >20} {: >20}".format(
-            i, mysqrt(i), math.sqrt(i), mysqrt(i) - math.sqrt(i)))
+        result = mysqrt(i)
+        print("{: >2} {: >20} {: >20} {: >25}".format(i, result, math.sqrt(i),
+                                                      result - math.sqrt(i)))
 
 
-test_square_root()
+# test_square_root()
+def silnia(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * silnia(n - 1)
+
+
+def eval_loop():
+    while True:
+        a = input('podaj dzialanie \n')
+        if a == 'gotowe':
+            print('gotowe')
+            break
+
+        print(eval(a))
+
+
+def calculate_skladnik(n):
+    return (silnia(4 * n) * (1103 + 26390 * n) / ((silnia(n)**4) * (396**
+                                                                    (4 * n))))
+
+
+def estimate_pi():
+    k = (2 * math.sqrt(2)) / 9801
+    n = 0
+    granica = 10**(-15)
+    result = 0
+
+    while True:
+        skladnik = calculate_skladnik(n)
+
+        if skladnik < granica:
+            break
+        else:
+            result += skladnik
+            n += 1
+
+    return 1 / (k * result)
